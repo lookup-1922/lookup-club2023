c3169c74ee3c601d0e3916c6b645c6fbd2279fe8
lookup <125456965+lookup-1922@users.noreply.github.com>
2023-08-17 11:51:15 +0900

    judgeのjavascript版

---
+++ Browser/judge.js
@@ -0,0 +1,38 @@
+function main() {
+    let number = window.prompt("完全数か調べる数字を入力してください。"); //入力ダイアログを表示
+
+    findDivisor(number);
+    let sumDivisor = 0; //合計を格納する変数
+    for (let i = 0; i < number.length; i++) {
+        sumDivisor += number[i]; // 配列の各要素を足し合わせる
+    }
+
+    if (number == sumDivisor) {
+        let result = window.confirm(number + "は完全数です。約数も表示しますか？");
+        if (result == true) {
+            window.alert(number + "は完全数です。約数は" + divisor + "です。");
+        }
+    } else if (number = !sumDivisor) {
+        let result = window.confirm(number + "は完全数ではありません。約数も表示しますか？");
+        if (result == true) {
+            window.alert(number + "は完全数ではありません。約数は" + divisor + "です。");
+        }
+    }
+}
+
+function findDivisor(number) { //約数を求める関数
+
+    let divisor = [];
+
+    if (number <= 0) {
+        return divisor; // 0以下の数には約数が存在しない
+    }
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            divisor.push(i); // 約数の場合、divisor配列に追加
+        }
+    }
+
+    return divisor;
+}
