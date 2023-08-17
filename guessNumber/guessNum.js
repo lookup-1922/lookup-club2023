ef548866556a82dbea85940112830acbca27c831
lookup <125456965+lookup-1922@users.noreply.github.com>
2023-08-17 21:42:13 +0900

    feat

--- guessNumber/guessNum.js
+++ guessNumber/guessNum.js
@@ -1,7 +1,14 @@
-function main(){
-    //code
+function main() {
+    // 0から99までのランダムな整数を生成
+    let randomNumber = Math.floor(Math.random() * 100);
+
+    // randomNumberが1桁の場合、前に0を追加して2桁にする
+    let formattedNumber = randomNumber.toString().padStart(2, '0');
+
+    // 変数numberに代入
+    let number = formattedNumber;
 }
 
-function reload(){
-    //code
+function reload() {
+    location.reload();
 }
